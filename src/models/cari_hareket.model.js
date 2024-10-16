b0fe0cbc18af254d9974125febb55812840318c2
alialaca <alialaca0@gmail.com>
2024-10-16 09:53:06 +0300

    feat(cari hareket): detay için model metodu tanımlandı

--- src/models/cari_hareket.model.js
+++ src/models/cari_hareket.model.js
@@ -18,6 +18,51 @@ class CariHareketModel {
             }
         })
     }
+
+    detail(id) {
+        return this.db['cariHareket'].findFirst({
+            where: {
+                id,
+                evrak_seri: {
+                    not: 'X'
+                }
+            },
+            select: {
+                id: true,
+                tarih: true,
+                evrak_seri: true,
+                evrak_sira: true,
+                evrak_tip: true,
+                evrak_cins: true,
+                normal_iade: true,
+                vade_tarihi: true,
+                borc_alacak: true,
+                ana_doviz_borc: true,
+                ana_doviz_alacak: true,
+                cari: {
+                    select: {
+                        kod: true,
+                        unvan: true
+                    }
+                },
+                stokHareket: {
+                    select: {
+                        miktar: true,
+                        tutar: true,
+                        vergi: true,
+                        iskonto1: true,
+                        iskonto2: true,
+                        stok: {
+                            select: {
+                                kod: true,
+                                isim: true
+                            }
+                        }
+                    }
+                }
+            }
+        })
+    }
 }
 
 module.exports = new CariHareketModel()
