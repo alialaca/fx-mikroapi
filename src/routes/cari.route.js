b26a6e71f91e0410088ffa09b570f7b3edffad30
alialaca <alialaca0@gmail.com>
2024-10-16 09:53:38 +0300

    feat(prisma.schema): Stok Hareket modeline ödeme planlı alanı eklendi

--- src/routes/cari.route.js
+++ src/routes/cari.route.js
@@ -6,6 +6,6 @@ router
     .get('/', Cari.list)
     .get('/:kod', Cari.find)
     .get('/:kod/hareketler', Cari.hareketler)
-    .get('/:kod/hareketler/:faturaId', Cari.hareketler)
+    .get('/:kod/hareketler/:faturaId', Cari.hareket)
 
 module.exports = router;
