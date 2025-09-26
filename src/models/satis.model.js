6f4d9addb82944883fb070b07bd1b86d7dce8035
alialaca <alialaca0@gmail.com>
2025-09-26 10:48:16 +0300

    feat(satis.model.js): Add sektor field to cari selection in query

--- src/models/satis.model.js
+++ src/models/satis.model.js
@@ -46,7 +46,8 @@ class SatisModel {
             cari: {
                 select: {
                     kod: true,
-                    unvan: true
+                    unvan: true,
+                    sektor: true
                 }
             },
             bolge: true,
